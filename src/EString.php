b0bac2e0107c01b54952a6838d2e5537c8877a66
Alexey Yashin <ayashin@extyl-pro.ru>
2021-03-09 17:53:43 +0300

    charset added

--- src/EString.php
+++ src/EString.php
@@ -5,15 +5,44 @@ namespace AlexeyYashin\EString;
 class EString
 {
     public $string = '';
+    protected $charset = null;
 
-    public function __construct($string = '')
+    /**
+     * EString constructor.
+     *
+     * @param string|\AlexeyYashin\EString\EString $string
+     * @param string|null                          $charset
+     */
+    public function __construct($string = '', string $charset = null)
     {
         $this->string = (string) $string;
+
+        if ($string instanceof EString && $charset === null) {
+            $this->charset = $string->charset;
+        } else {
+            $this->charset = $charset;
+        }
     }
 
+    /**
+     * @param $new_string
+     *
+     * @return \AlexeyYashin\EString\EString
+     */
+    protected function getNew($new_string)
+    {
+        return new self($new_string, $this->getCharset());
+    }
+
+    /**
+     * @param string|\AlexeyYashin\EString\EString $substr
+     * @param bool                                 $i
+     *
+     * @return bool
+     */
     public function startsWith($substr, $i = false): bool
     {
-        $substr = new self($substr);
+        $substr = $this->getNew($substr);
         $string = $this;
 
         if ($i) {
@@ -21,17 +50,50 @@ class EString
             $substr = $substr->toLowerCase();
         }
 
-        return mb_strpos($string, $substr) === 0;
+        return mb_strpos($string, $substr, 0, $this->getCharset()) === 0;
     }
 
+    /**
+     * @param string $charset
+     *
+     * @return $this
+     */
+    public function charset(string $charset)
+    {
+        $this->charset = $charset;
+
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getCharset()
+    {
+        if ($this->charset === null) {
+            $this->charset = mb_internal_encoding();
+        }
+
+        return $this->charset;
+    }
+
+    /**
+     * @return \AlexeyYashin\EString\EString
+     */
     public function toLowerCase()
     {
-        return new self(mb_strtolower($this->string));
+        return $this->getNew(mb_strtolower($this->string, $this->getCharset()));
     }
 
+    /**
+     * @param string|\AlexeyYashin\EString\EString $substr
+     * @param bool                                 $i
+     *
+     * @return bool
+     */
     public function endsWith($substr, $i = false)
     {
-        $substr = new self($substr);
+        $substr = $this->getNew($substr);
         $string = $this;
 
         if ($i) {
@@ -39,12 +101,20 @@ class EString
             $substr = $substr->toLowerCase();
         }
 
-        return $substr->equals(mb_substr($string, -$substr->length()));
+        return $substr == $string->substring(-$substr->length());
     }
 
+    /**
+     * @deprecated use == instead
+     *
+     * @param      $string
+     * @param bool $strict
+     *
+     * @return bool
+     */
     public function equals($string, $strict = true)
     {
-        $string = new self($string);
+        $string = $this->getNew($string);
 
         return (
             (
@@ -57,11 +127,20 @@ class EString
         );
     }
 
+    /**
+     * @return int
+     */
     public function length()
     {
-        return (int) mb_strlen($this->string);
+        return (int) mb_strlen($this->string, $this->getCharset());
     }
 
+    /**
+     * @param      $search
+     * @param null $replace
+     *
+     * @return \AlexeyYashin\EString\EString
+     */
     public function replace($search, $replace = null)
     {
         if (
@@ -72,37 +151,50 @@ class EString
             $search = array_keys($replace);
         }
 
-        return new self(str_replace($search, $replace, $this->string));
+        return $this->getNew(str_replace($search, $replace, $this->string));
     }
 
+    /**
+     * @param string $charlist
+     *
+     * @return \AlexeyYashin\EString\EString
+     */
     public function trim($charlist = " \t\n\r\0\x0B")
     {
-        return new self(trim($this->string, $charlist));
+        return $this->getNew(trim($this->string, $charlist));
     }
 
+    /**
+     * @return \AlexeyYashin\EString\EString
+     */
     public function toCamelCase()
     {
         $str = '';
-        foreach ($this->caseArray() as $word) {
-            $str .= ucfirst($word);
+        foreach ($this->getCaseArray() as $word) {
+            $str .= $word->ucfirst();
         }
 
-        return new self($str);
+        return $this->getNew($str);
     }
 
+    /**
+     * @param bool $toupper
+     *
+     * @return array
+     */
     protected function caseArray($toupper = false)
     {
         $str = preg_replace_callback('/[A-Z][a-z]/', function($m)
         {
-            return '_' . mb_strtolower($m[0]);
+            return '_' . mb_strtolower($m[0], $this->getCharset());
         }, $this->string);
         $str = preg_replace_callback('/([A-Z]{2,})/', function($m)
         {
-            return sprintf('_%s_', mb_strtolower($m[0]));
+            return sprintf('_%s_', mb_strtolower($m[0], $this->getCharset()));
         }, $str);
         $str = preg_replace('/[A-Z]/', '_$0', $str);
 
-        $str = (new EString($str));
+        $str = $this->getNew($str);
         $str = $toupper ? $str->toUpperCase() : $str->toLowerCase();
 
         $str = preg_split('/[^a-zA-Z0-9]+/', $str);
@@ -110,11 +202,33 @@ class EString
         return array_filter($str);
     }
 
-    public function toUpperCase()
+    /**
+     * @param bool $to_upper
+     *
+     * @return EString[]
+     */
+    public function getCaseArray(bool $to_upper = false)
     {
-        return new self(mb_strtoupper($this->string));
+        return array_map(
+            function($item)
+            {
+                return $this->getNew($item);
+            },
+            $this->caseArray($to_upper)
+        );
     }
 
+    /**
+     * @return \AlexeyYashin\EString\EString
+     */
+    public function toUpperCase()
+    {
+        return $this->getNew(mb_strtoupper($this->string, $this->getCharset()));
+    }
+
+    /**
+     * @return \AlexeyYashin\EString\EString
+     */
     public function toLowerCamelCase()
     {
         $words = $this->caseArray();
@@ -125,19 +239,59 @@ class EString
             $str .= ucfirst($word);
         }
 
-        return new self($str);
+        return $this->getNew($str);
     }
 
+    /**
+     * @return \AlexeyYashin\EString\EString
+     */
     public function toSnakeCase()
     {
-        return new self(implode('_', $this->caseArray()));
+        return $this->getNew(implode('_', $this->caseArray()));
     }
 
+    /**
+     * @return \AlexeyYashin\EString\EString
+     */
     public function toUpperSnakeCase()
     {
-        return new self(implode('_', $this->caseArray(true)));
+        return $this->getNew(implode('_', $this->caseArray(true)));
     }
 
+    /**
+     * @return \AlexeyYashin\EString\EString
+     */
+    public function ucfirst()
+    {
+        if (function_exists('mb_ucfirst')) {
+            $result = mb_ucfirst($this);
+        } else {
+            $letter = $this->substring(0, 1)->toUpperCase();
+            $suffix = $this->substring(1, $this->length() - 1);
+            $result = $letter . $suffix;
+        }
+
+        return $this->getNew($result);
+    }
+
+    /**
+     * @param      $start
+     * @param null $length
+     *
+     * @return \AlexeyYashin\EString\EString
+     */
+    public function substring($start, $length = null)
+    {
+        return $this->getNew(mb_substr($this, $start, $length, $this->getCharset()));
+    }
+
+    /**
+     * @param        $array
+     * @param        $key
+     * @param string $delimeter
+     *
+     * @return array|null
+     */
     protected static function getValueFromArray($array, $key, $delimeter = '.')
     {
         $current_value = $array;
@@ -167,7 +321,7 @@ class EString
 
     /**
      * @param object|array $vars
-     * @param string $group_delimeter Deprecated
+     * @param string       $group_delimeter Deprecated
      *
      * @return \AlexeyYashin\EString\EString
      */
@@ -193,7 +347,7 @@ class EString
                         return htmlspecialchars($value);
                     default:
                         if (
-                            mb_strlen($matches['mod'])
+                            mb_strlen($matches['mod'], $this->getCharset())
                             && function_exists($matches['mod'])
                         ) {
                             return call_user_func($matches['mod'], $value);
@@ -205,27 +359,45 @@ class EString
             $this
         );
 
-        return new self($newString);
+        return $this->getNew($newString);
     }
 
+    /**
+     * @param      $substr
+     * @param bool $i
+     *
+     * @return \AlexeyYashin\EString\EString
+     */
     public function cropStart($substr, $i = false)
     {
         if ($this->startsWith($substr, $i)) {
-            return new self(substr($this, mb_strlen($substr)));
+            return $this->getNew($this->substring(mb_strlen($substr)));
         }
 
-        return new self($this);
+        return $this->getNew($this);
     }
 
+    /**
+     * @param      $substr
+     * @param bool $i
+     *
+     * @return \AlexeyYashin\EString\EString
+     */
     public function cropEnd($substr, $i = false)
     {
         if ($this->endsWith($substr, $i)) {
-            return new self(substr($this, 0, - mb_strlen($substr)));
+            return $this->getNew($this->substring(0, -mb_strlen($substr)));
         }
 
-        return new self($this);
+        return $this->getNew($this);
     }
 
+    /**
+     * @param      $substr
+     * @param bool $i
+     *
+     * @return \AlexeyYashin\EString\EString
+     */
     public function crop($substr, $i = false)
     {
         return $this
